9d2b9abe44bf9646a9d4322fcef2738dd321048c
kowczanka <kowczanka@gmail.com>
2020-08-30 18:24:03 +0200

    Wersja poprawiona

---
+++ zadanie_kostka.py
@@ -0,0 +1,33 @@
+import random
+
+def roll_the_dice():
+
+    """ Założenia:
+        W tym zadaniu stworzymy funkcję obliczającą wynik rzutu kostkami do gry.
+        Na wejściu musimy podać ciąg znaków opisujący nasz rzut. Na przykład: "2D6+6".
+        Oznacza to rzut dwiema kostkami sześcio-ściennymi powiększony o 5. """
+
+    # wypisujemy informacje z danych:
+
+    data = "5D6+6"
+    number_of_dices = int(data[0])
+    walls = int(data[2])
+    add = int(data[4])
+
+    # podajemy jakie są warunki gry
+
+    print("Number of dices:", number_of_dices, f"Dices have {walls} walls.", f"We will add: {add} to score")
+
+    # obliczamy wynik w punktach:
+
+    result = []
+    for x in range(number_of_dices):
+        value_of_one_throw = random.randint(1, walls)
+        result.append(value_of_one_throw)
+    print("You've reached score: ", result)
+    result.append(add)
+    print("Result of the game: ", sum(result))
+    return
+
+
+roll_the_dice()
